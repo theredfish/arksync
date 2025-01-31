23d02bb0c604aef4f1cf18e5e07fd27b00a8377c
theredfish <did.julian@gmail.com>
2025-02-01 00:19:05 +0100

    Add router and main links

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,38 @@
 use crate::components::charts::DynamicChartExample;
 use crate::components::sidebar::Sidebar;
 use leptos::prelude::*;
+use leptos_router::{
+    components::{Route, Router, Routes},
+    path,
+};
 
 #[component]
 pub fn App() -> impl IntoView {
     view! {
-        <div class="bg-darcula-black text-gray-100 flex w-screen h-screen">
-            <Sidebar class="w-1/6 bg-darcula-gray p-5" />
-            <div id="charts-container" class="w-full p-5">
-                <DynamicChartExample />
-            </div>
+        <Router>
+            <main class="bg-darcula-black text-gray-100 flex w-screen h-screen">
+                <Sidebar class="w-1/6 bg-darcula-gray p-5" />
+                <Routes fallback=|| "Not found.">
+                    <Route path=path!("/") view=Home/>
+                    <Route path=path!("/dashboard") view=Dashboard />
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+#[component]
+pub fn Dashboard() -> impl IntoView {
+    view! {
+        <div id="charts-container" class="w-full p-5">
+            <DynamicChartExample />
         </div>
+    }
+}
+
+#[component]
+pub fn Home() -> impl IntoView {
+    view! {
+        <p>Welcome to ArkSync</p>
     }
 }
--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -1,11 +1,15 @@
 use leptos::prelude::*;
 use leptos::IntoView;
+use leptos_router::components::A;
 
 #[component]
 pub fn Sidebar(#[prop(into)] class: String) -> impl IntoView {
     view! {
         <div class={class}>
-            <h2>"ArkSync"</h2>
+            <ul>
+                <li><A href="/">"ArkSync"</A></li>
+                <li><A href="dashboard">"Dashboard"</A></li>
+            </ul>
         </div>
     }
 }
