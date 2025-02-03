ce3bdab06330e15fbce43c37c676f1d292db46d7
theredfish <did.julian@gmail.com>
2025-02-03 23:59:22 +0100

    Add style for sidebar menus with heroicons

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,2 +1,3 @@
 pub mod charts;
+pub mod heroicons;
 pub mod sidebar;
--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -1,3 +1,4 @@
+use crate::components::heroicons::PresentationChartBarIcon;
 use leptos::prelude::*;
 use leptos::IntoView;
 use leptos_router::components::A;
@@ -6,9 +7,14 @@ use leptos_router::components::A;
 pub fn Sidebar(#[prop(into)] class: String) -> impl IntoView {
     view! {
         <div class={class}>
-            <ul>
+            <ul class="space-y-3">
                 <li><A href="/">"ArkSync"</A></li>
-                <li><A href="dashboard">"Dashboard"</A></li>
+                <li><A href="dashboard">
+                    <span class="inline-flex items-center">
+                        <PresentationChartBarIcon class="h-6 w-6 mr-1" />
+                        "Dashboard"
+                    </span>
+                </A></li>
             </ul>
         </div>
     }
