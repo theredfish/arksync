b157ff103762f2b6979fc6196e0d488c3d9be8ef
theredfish <juliandidier@protonmail.com>
2025-04-25 23:24:34 +0200

    Implement resize animations

--- src/app.rs
+++ src/app.rs
@@ -11,7 +11,7 @@ use leptos_router::{
 pub fn App() -> impl IntoView {
     view! {
         <Router>
-            <main class="bg-darcula-black text-gray-100 flex min-h-screen">
+            <main class="bg-darcula-black text-gray-100 flex min-h-screen antialiased">
                 <Sidebar class="w-2/12 bg-darcula-gray p-5" />
                 <Routes fallback=|| "Not found.">
                     <Route path=path!("/") view=Home/>
@@ -27,13 +27,13 @@ pub fn Dashboards() -> impl IntoView {
     view! {
         <div class="w-full p-4">
             <Grid>
-                <GridItem id=1 width=2 height=5 position_x=1 position_y=5>
+                <GridItem id=1 width=300 height=300 position_x=400 position_y=100>
                     <AirTemperatureGauge />
                 </GridItem>
-                <GridItem id=2 width=2 height=12 position_x=10 position_y=5>
+                <GridItem id=2 width=800 height=400 position_x=500 position_y=200>
                     <WaterTemperatureChart />
                 </GridItem>
-                <GridItem id=3 width=1 height=3 position_x=0 position_y=3>
+                <GridItem id=3 width=100 height=100 position_x=0 position_y=0>
                     No data yet
                 </GridItem>
             </Grid>
--- src/components/grid/grid_component.rs
+++ src/components/grid/grid_component.rs
@@ -20,7 +20,7 @@ pub fn Grid(children: Children) -> impl IntoView {
     );
 
     view! {
-        <div class="h-full grid grid-cols-12 grid-rows-[repeat(auto-fill,minmax(50px,auto))] gap-4">
+        <div class="h-full">
             { children() }
         </div>
     }
