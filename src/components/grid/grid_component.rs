a864b33634c60e8f982c9c897bab536991adb58b
theredfish <juliandidier@protonmail.com>
2025-05-02 22:47:04 +0200

    Apply drag boundaries to the dragged component
    We subscribe to the parent boundaries and reactively clamp the
    grid item drag event. This prevents to go outside of the grid.

--- src/components/grid/grid_component.rs
+++ src/components/grid/grid_component.rs
@@ -1,25 +1,40 @@
-use leptos::{logging::log, prelude::*};
+use crate::components::grid::{GridBounding, GridContext};
+use leptos::{html::Div, logging::log, prelude::*};
+use leptos_use::{use_element_bounding, UseElementBoundingReturn};
 use std::collections::HashMap;
-
-use crate::components::grid::GridStorage;
 
 #[component]
 pub fn Grid(children: Children) -> impl IntoView {
-    let grid_storage = GridStorage {
-        items: HashMap::new(),
+    let grid_layout = NodeRef::<Div>::new();
+    let grid_context = GridContext {
+        storage: HashMap::new(),
+        boundaries: GridBounding {
+            width: 0.,
+            height: 0.,
+        },
     };
-    let update_grid = RwSignal::new(grid_storage);
+    let grid_ctx = RwSignal::new(grid_context);
 
-    provide_context(update_grid);
+    provide_context(grid_ctx);
 
-    Effect::watch(
-        move || update_grid.get(),
-        move |storage: &GridStorage, _prev, _| log!("[Grid] storage: {storage:#?}"),
-        false,
-    );
+    Effect::new(move || {
+        let UseElementBoundingReturn { width, height, .. } = use_element_bounding(grid_layout);
+        grid_ctx.update(|ctx| {
+            ctx.boundaries = GridBounding {
+                width: width.get_untracked(),
+                height: height.get_untracked(),
+            };
+        });
+    });
+
+    // Effect::watch(
+    //     move || grid_ctx.get(),
+    //     move |ctx: &GridContext, _prev, _| log!("[Grid] context: {ctx:#?}"),
+    //     false,
+    // );
 
     view! {
-        <div class="relative h-full border border-red-300">
+        <div node_ref=grid_layout class="relative h-full border border-red-300">
             { children() }
         </div>
     }
