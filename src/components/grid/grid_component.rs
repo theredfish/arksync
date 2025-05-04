a8cd8627ed7736bb672a038decce9cd6d2f916c0
theredfish <juliandidier@protonmail.com>
2025-05-04 20:44:56 +0200

    Watch grid boundaries

--- src/components/grid/grid_component.rs
+++ src/components/grid/grid_component.rs
@@ -1,5 +1,5 @@
 use crate::components::grid::{GridBounding, GridContext};
-use leptos::{html::Div, logging::log, prelude::*};
+use leptos::{html::Div, prelude::*};
 use leptos_use::{use_element_bounding, UseElementBoundingReturn};
 use std::collections::HashMap;
 
@@ -17,21 +17,19 @@ pub fn Grid(children: Children) -> impl IntoView {
 
     provide_context(grid_ctx);
 
-    Effect::new(move || {
-        let UseElementBoundingReturn { width, height, .. } = use_element_bounding(grid_layout);
-        grid_ctx.update(|ctx| {
-            ctx.boundaries = GridBounding {
-                width: width.get_untracked(),
-                height: height.get_untracked(),
-            };
-        });
-    });
-
-    // Effect::watch(
-    //     move || grid_ctx.get(),
-    //     move |ctx: &GridContext, _prev, _| log!("[Grid] context: {ctx:#?}"),
-    //     false,
-    // );
+    let UseElementBoundingReturn { width, height, .. } = use_element_bounding(grid_layout);
+    Effect::watch(
+        move || (width.get(), height.get()),
+        move |(width, height): &(f64, f64), _, _| {
+            grid_ctx.update(|ctx| {
+                ctx.boundaries = GridBounding {
+                    width: *width,
+                    height: *height,
+                };
+            });
+        },
+        false,
+    );
 
     view! {
         <div node_ref=grid_layout class="relative h-full border border-red-300">
