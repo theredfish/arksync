cf56d2d5dbfa098bad96d1113eb424f1c8352195
theredfish <juliandidier@protonmail.com>
2026-05-04 01:01:23 +0200

    Update license to MPL-2.0 (#31)
    * Update license to MPL-2.0
    
    Fix #30
    
    * chore: add MPL headers to Rust files

--- crates/ui/src/components/grid/core/layout.rs
+++ crates/ui/src/components/grid/core/layout.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use crate::components::grid::core::collision::{grid as collision_grid, item_aabb};
 use crate::components::grid::core::drop_placement::{self, DropPlacement};
 use crate::components::grid::core::item::Axes;
