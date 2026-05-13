ce617417aec6fbb3133c2f9346ef91726ee80c0b
theredfish <juliandidier@protonmail.com>
2026-05-14 01:50:10 +0200

    Mark db migration test as integration ignored

--- crates/db/tests/postgres.rs
+++ crates/db/tests/postgres.rs
@@ -2,6 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+#[ignore = "Integration test"]
 #[test]
 fn connects_and_runs_migrations() {
     let runtime = tokio::runtime::Runtime::new().expect("runtime");
